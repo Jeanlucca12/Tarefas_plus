24b6b11e5ad1aecb94885171be25fb0378b11e41
jean <jeanluccabueno@gmail.com>
2024-10-22 16:22:53 -0300

    connections

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -3,13 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQaj_IYjNF5MbwIZHpqZmrxu1NLNSQA7c",
-  authDomain: "tarefasplus-1f7b6.firebaseapp.com",
-  projectId: "tarefasplus-1f7b6",
-  storageBucket: "tarefasplus-1f7b6.appspot.com",
-  messagingSenderId: "997221023259",
-  appId: "1:997221023259:web:69b026a523da28e0fc82be"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
